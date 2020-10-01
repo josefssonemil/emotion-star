9ef5f0402b14b5c32eef4c052330907f1b59a5b4
Mattias <mattiashallin93@gmail.com>
2020-10-01 13:08:09 +0200

    Played with game field

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -1,8 +1,31 @@
+import { motion } from "framer-motion";
+
 export default function ProgressBar(props) {
   return (
-    <div className="w-screen h-8">
-      <div className=""></div>
-      Progress Bar
+    <div
+      /* Wrapper */
+      className="w-full h-4 relative"
+    >
+      <div
+        /* Grey bar background */
+        className="w-full h-2 bg-yellow-200  bg-opacity-25 absolute inset-0 z-0 my-1"
+      />
+      <div
+        /* Progress Bar */
+        style={{
+          width: props.position,
+          boxShadow: "0 0 25px 1px #f6e05e",
+        }}
+        className="h-2 bg-yellow-400 absolute inset-y-0 left-0 my-1"
+      />
+      <div
+        /* Progress Dot */
+        style={{
+          left: props.position,
+          boxShadow: "0 0 25px 2px #f6e05e",
+        }}
+        className="h-4 w-4 border-2 absolute inset-y-0 rounded-full border-yellow-400 border-opacity-25 bg-yellow-100 shadow-base shadow-inner"
+      />
     </div>
   );
 }
--- components/screens/GameScreen.tsx
+++ components/screens/GameScreen.tsx
@@ -2,6 +2,7 @@ import { MutableRefObject } from "react";
 import { Expression } from "../../types/Expressions";
 import PlayerFace from "../PlayerFace";
 import ProgressBar from "../ProgressBar";
+import PlayField from "../PlayField";
 
 interface Props {
   canvasLeftRef: MutableRefObject<HTMLCanvasElement>;
@@ -12,23 +13,28 @@ interface Props {
 
 export default function GameScreen(props: Props) {
   return (
-    <div className="flex flex-col h-full justify-around">
-      <div className="flex flex-row justify-start pl-24">
-        <PlayerFace
-          canvasRef={props.canvasLeftRef}
-          expression={props.players[0]}
-          faceBox={props.faceBoxes[0]}
-        />
-        <div>Playfield: Player 1</div>
+    <div className="flex flex-col h-full justify-around bg-gray-700">
+      <div className="flex flex-row justify-start pl-4">
+        <div className="w-1/6 z-50">
+          <PlayerFace
+            canvasRef={props.canvasLeftRef}
+            expression={props.players[0]}
+            faceBox={props.faceBoxes[0]}
+          />
+        </div>
+        <PlayField />
       </div>
-      <ProgressBar />
-      <div className="flex flex-row justify-start pl-24">
-        <PlayerFace
-          canvasRef={props.canvasRightRef}
-          expression={props.players[1]}
-          faceBox={props.faceBoxes[1]}
-        />
-        <div>Playfield: Player 2</div>
+      <ProgressBar position={"50%"} />
+      <div className="flex flex-row justify-start pl-4">
+        <div className="w-1/6 z-50">
+          <PlayerFace
+            canvasRef={props.canvasRightRef}
+            expression={props.players[1]}
+            faceBox={props.faceBoxes[1]}
+          />
+        </div>
+
+        <PlayField />
       </div>
     </div>
   );
