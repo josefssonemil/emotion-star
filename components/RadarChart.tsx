cd56a82b9871ac6b1eb9c06487755526f4c7fbc6
Mattias <mattiashallin93@gmail.com>
2020-10-07 17:37:20 +0200

    Re-styled radar chart

--- components/RadarChart.tsx
+++ components/RadarChart.tsx
@@ -1,36 +1,88 @@
 import React from "react";
 import { Radar, defaults } from "react-chartjs-2";
 import "chartjs-plugin-style";
+import useMeasure from "react-use-measure";
 
 export default function RadarChart(props) {
   const performance = props.performance;
+  const [ref, bounds] = useMeasure();
+  let width: number;
+  let height: number;
+  height = bounds.height;
+  width = bounds.height;
+
   const data = {
     /*["😁", "😡", "😥", "😐", "😮"]*/
     labels: ["", "", "", "", ""],
     datasets: [
       {
         label: "Player 1",
-        backgroundColor: "rgba(75, 250, 240, .3)",
+
         borderColor: "rgba(75, 250, 240, 1)",
-        pointRadius: 0,
+        backgroundColor: "rgba(75, 250, 240, .2)",
+
+        outerGlowWidth: 10,
+        outerGlowColor: "rgba(75, 250, 240, 1)",
+        innerGlowWidth: 5,
+        innerGlowColor: "rgba(75, 250, 240, 1)",
+
+        pointRadius: 6,
+        pointBorderWidth: 1,
+        pointBorderColor: "rgba(75, 250, 240, 1)",
+        pointBackgroundColor: "rgba(255,255,255, 1)",
+        pointOuterGlowColor: "rgba(75, 250, 240, 1)",
+        pointOuterGlowWidth: 5,
+        pointInnerGlowColor: "rgba(255, 255, 255, 1)",
+        pointInnerGlowWidth: 50,
+
         borderWidth: 3,
         lineTension: 0,
         data: performance.player1,
       },
       {
         label: "Player 2",
-        backgroundColor: "rgba(202, 250, 75, .3)",
+
         borderColor: "rgba(202, 250, 75, 1)",
-        pointRadius: 0,
+        backgroundColor: "rgba(202, 250, 75, .2)",
+
+        outerGlowWidth: 10,
+        outerGlowColor: "rgba(202, 250, 75, 1)",
+        innerGlowWidth: 5,
+        innerGlowColor: "rgba(202, 250, 75, 1)",
+
+        pointRadius: 6,
+        pointBorderWidth: 1,
+        pointBorderColor: "rgba(202, 250, 75, 1)",
+        pointBackgroundColor: "rgba(255,255,255, 1)",
+        pointOuterGlowColor: "rgba(202, 250, 75, 1)",
+        pointOuterGlowWidth: 5,
+        pointInnerGlowColor: "rgba(255, 255, 255, 1)",
+        pointInnerGlowWidth: 50,
+
         borderWidth: 3,
         lineTension: 0,
         data: performance.player2,
       },
       {
         label: "Average",
-        backgroundColor: "rgba(179,181,198,0.2)",
-        borderColor: "rgba(179,181,198,.5)",
-        pointRadius: 0,
+
+        borderColor: "rgba(255, 0, 255, 1)",
+        backgroundColor: "rgba(255, 0, 255, .2)",
+
+        outerGlowWidth: 10,
+        outerGlowColor: "rgba(255, 0, 255, 1)",
+        innerGlowWidth: 5,
+        innerGlowColor: "rgba(255, 0, 255, 1)",
+
+        pointRadius: 6,
+        pointBorderWidth: 1,
+        pointBorderColor: "rgba(255, 0, 255, 1)",
+        pointBackgroundColor: "rgba(255,255,255, 1)",
+        pointOuterGlowColor: "rgba(255, 0, 255, 1)",
+        pointOuterGlowWidth: 5,
+        pointInnerGlowColor: "rgba(255, 255, 255, 1)",
+        pointInnerGlowWidth: 50,
+
         borderWidth: 3,
         lineTension: 0,
         data: performance.average,
@@ -40,6 +92,7 @@ export default function RadarChart(props) {
 
   const options = {
     maintainAspectRatio: false,
+    responsive: true,
     legend: {
       display: false,
     },
@@ -49,42 +102,196 @@ export default function RadarChart(props) {
       },
       angleLines: {
         display: true,
-        color: "rgba(70, 2, 112, .1)",
+        color: "rgba(255, 255, 255, .2)",
       },
       gridLines: {
         circular: true,
-        display: true,
-        color: "rgba(70, 2, 112, .1)",
+        display: false,
+        color: "rgba(70, 2, 112, .3)",
       },
       ticks: {
         display: false,
         suggestedMin: 0,
         suggestedMax: 100,
-        stepSize: 20,
+        stepSize: 25,
       },
+    },
+    layout: {
+      padding: 50,
     },
   };
   return (
-    <div className="flex w-56 h-56 self-center relative">
-      <div className="absolute top-0 inset-x-0 flex justify-center">
-        <div className="text-3xl flex">😁</div>
+    <div
+      ref={ref}
+      style={{ width, height: "100%" }}
+      className="flex self-center relative"
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: "50%",
+        }}
+        className="text-5xl -ml-6"
+      >
+        😁
       </div>
-      <div className="absolute inset-0 flex content-center">
-        <div className="flex self-center justify-center space-x-40 w-full h-24">
-          <div className="text-3xl">😮</div>
-          <div className="text-3xl">😡</div>
-        </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "32%",
+          right: 0,
+        }}
+        className="text-5xl"
+      >
+        😮
       </div>
-      <div className="absolute bottom-0 inset-x-0 flex justify-center space-x-20">
-        <div className="text-3xl flex">😥</div>
-        <div className="text-3xl flex">😐</div>
+      <div
+        style={{
+          position: "absolute",
+          bottom: "2%",
+          right: "17.5%",
+        }}
+        className="text-5xl"
+      >
+        😡
       </div>
-      <div className="h-48 w-48 p-4 m-auto">
-        <Radar data={data} options={options} />
+      <div
+        style={{
+          position: "absolute",
+          bottom: "2%",
+          left: "17.5%",
+        }}
+        className="text-5xl"
+      >
+        😥
       </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "32%",
+          left: 0,
+        }}
+        className="text-5xl"
+      >
+        😐
+      </div>
+      <div
+        style={{
+          marginLeft: "58px",
+          marginRight: "58px",
+          marginTop: "70px",
+          marginBottom: "56px",
+        }}
+        className="absolute inset-0 bg-black bg-opacity-25 rounded-full"
+      />
+      <div
+        style={{
+          paddingLeft: "50px",
+          paddingRight: "50px",
+          paddingTop: "62px",
+          paddingBottom: "48px",
+        }}
+        className="absolute inset-0 flex justify-center items-center "
+      >
+        <Circle size={100} />
+      </div>
+      <div
+        style={{
+          paddingLeft: "50px",
+          paddingRight: "50px",
+          paddingTop: "62px",
+          paddingBottom: "48px",
+        }}
+        className="absolute inset-0 flex justify-center items-center"
+      >
+        <Circle size={75} />
+      </div>
+      <div
+        style={{
+          paddingLeft: "50px",
+          paddingRight: "50px",
+          paddingTop: "62px",
+          paddingBottom: "48px",
+        }}
+        className="absolute inset-0 flex justify-center items-center"
+      >
+        <Circle size={50} />
+      </div>
+      <div
+        style={{
+          paddingLeft: "50px",
+          paddingRight: "50px",
+          paddingTop: "62px",
+          paddingBottom: "48px",
+        }}
+        className="absolute inset-0 flex justify-center items-center"
+      >
+        <Circle size={25} />
+      </div>
+      <Radar data={data} options={options} />
     </div>
   );
 }
+const Circle = (props) => {
+  return (
+    <svg
+      style={{
+        width: props.size + "%",
+        height: props.size + "%",
+      }}
+      className="flex opacity-75"
+      viewBox="0 0 576 576"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_d)">
+        <circle
+          cx="288.001"
+          cy="287.908"
+          r="281.438"
+          stroke="rgba(255,255,255, .3)"
+          stroke-width="2"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d"
+          x="0.5625"
+          y="0.469845"
+          width="574.876"
+          height="574.876"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          />
+          <feOffset />
+          <feGaussianBlur stdDeviation="2.5" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.366667 0 0 0 0 1 0 0 0 0 0.962 0 0 0 1 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
 /*
 <div className="flex w-56 h-56 self-center relative">
       <div className="absolute top-0 inset-x-0 flex justify-center">
--- components/screens/FinalScreen.tsx
+++ components/screens/FinalScreen.tsx
@@ -12,13 +12,12 @@ const data = {
   },
 };
 export default function FinalScreen() {
-
   const textGlow = {
-    textShadow: "0 0 35px rgb(255, 0, 255)"
+    textShadow: "0 0 35px rgb(255, 0, 255)",
   };
 
   const borderGlow = {
-    boxShadow: "0 0 rgb(255, 0, 255)"
+    boxShadow: "0 0 rgb(255, 0, 255)",
   };
 
   return (
@@ -26,30 +25,19 @@ export default function FinalScreen() {
       style={{ backgroundImage: "url('/img/startscreen-bg.jpg')" }}
       className="h-screen w-full bg-center bg-cover"
     >
-
-      <h1 className="text-6xl p-6 text-white"
-        style={textGlow}>
+      <h1 className="text-6xl p-6 text-white" style={textGlow}>
         Expression Score
       </h1>
 
-
       <div className="flex flex-row mx-auto justify-around">
-
-        <div className="w-4/12 bg-red-700">
-
+        <div className="w-4/12">
           <RadarChart performance={data.performance} />
-
-
         </div>
 
         <div className="w-7/12 " style={borderGlow}>
-
           <div className="flex flex-row ">
-
             <div className="w-2/3 flex flex-col justify-around ">
-
               <div className="flex flex-row flex-1">
-
                 <div className="w-1/3 p-6">
                   <img src="https://raw.githubusercontent.com/julien-gargot/images-placeholder/master/placeholder-portrait.png"></img>
                 </div>
@@ -58,21 +46,15 @@ export default function FinalScreen() {
                   <div className="flex flex-col  p-6">
                     <h1 className="text-3xl">Player One</h1>
 
-                    <div>
-                      Item1
-                      </div>
+                    <div>Item1</div>
                     <div>Item2</div>
                     <div>Item3</div>
                     <div>Item4</div>
                     <div>Item5</div>
-
                   </div>
                 </div>
-
-
               </div>
               <div className="flex flex-row flex-1">
-
                 <div className="w-1/3 p-6">
                   <img src="https://raw.githubusercontent.com/julien-gargot/images-placeholder/master/placeholder-portrait.png"></img>
                 </div>
@@ -81,44 +63,28 @@ export default function FinalScreen() {
                   <div className="flex flex-col  p-6">
                     <h1 className="text-3xl">Player Two</h1>
 
-                    <div>
-                      Item1
-                      </div>
+                    <div>Item1</div>
                     <div>Item2</div>
                     <div>Item3</div>
                     <div>Item4</div>
                     <div>Item5</div>
-
                   </div>
                 </div>
-
-
               </div>
-
             </div>
-
 
             <div className="w-1/3 flex flex-col justify-between text-right ">
-              <h1 className="text-white text-3xl" style={textGlow}>Team: 🌭</h1>
+              <h1 className="text-white text-3xl" style={textGlow}>
+                Team: 🌭
+              </h1>
 
-              <h1 className="text-white text-6xl" style={textGlow}>3250p</h1>
+              <h1 className="text-white text-6xl" style={textGlow}>
+                3250p
+              </h1>
             </div>
-
-
-
           </div>
-
-
-
         </div>
-
-
       </div>
-
-
-
-
-
     </div>
   );
 }
