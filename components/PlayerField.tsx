c342630838b1606b99ae730ad38e465564001545
Jesper Lindström <jesper.lindstrom@inly.se>
2020-10-15 11:50:51 +0200

    Revert "Progress on stats"
    This reverts commit a51ac6684d468ea41424f5ef1c756b82c36656dc.

--- components/PlayerField.tsx
+++ components/PlayerField.tsx
@@ -16,6 +16,8 @@ interface Props {
 export default function PlayerField(props: Props) {
   const x = useMotionValue(0);
 
+  console.log(-props.duration * gameConstants.pixelsPerSecond);
+
   useEffect(() => {
     const controls = animate(
       x,
