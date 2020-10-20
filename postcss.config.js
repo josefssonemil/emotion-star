cf337dc7fa14485a4877bb250f2d9ac88e98d6fa
Jesper Lindström <jesper.lindstrom@inly.se>
2020-10-20 14:14:56 +0200

    Fixed purgecss issue

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,3 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    ...(process.env.NODE_ENV === 'production' ? {
-      '@fullhuman/postcss-purgecss': {
-        // added sections folder and changed extension to jsx
-        content: [
-          './pages/**/*.{js,jsx,ts,tsx}',
-          './components/**/*.{js,jsx,ts,tsx}',
-        ],
-        defaultExtractor: content =>
-          content.match(/[\w-/:]+(?<!:)/g) || [],
-      },
-    } : {}),
-  },
+  plugins: ["tailwindcss", "autoprefixer"],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,27 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: {
+    content: [
+      "./pages/**/*.{js,jsx,ts,tsx}",
+      "./components/**/*.{js,jsx,ts,tsx}",
+    ],
+
+    options: {
+      whitelist: [
+        "row-start-1",
+        "row-start-2",
+        "row-start-3",
+        "row-start-4",
+        "row-start-5",
+      ],
+    },
+  },
   theme: {
     extend: {
-      height: (theme) => ({
+      height: {
         "screen-2/3": "66vh",
         "screen/2": "50vh",
         "screen/3": "calc(100vh / 3)",
@@ -16,8 +29,8 @@ module.exports = {
         "screen/5": "calc(100vh / 5)",
         100: "25rem",
         warmupCam: "25vh",
-      }),
-      height: (theme) => ({
+      },
+      height: {
         "screen-2/3": "66vw",
         "screen/2": "50vw",
         "screen/3": "calc(100vw / 3)",
@@ -25,7 +38,7 @@ module.exports = {
         "screen/5": "calc(100vw / 5)",
         100: "25rem",
         88: "22rem",
-      }),
+      },
       colors: {
         local: {
           purple1: "#460270",
@@ -36,11 +49,12 @@ module.exports = {
       },
       fontFamily: {
         luckiest: ["Luckiest Guy", "sans-serif"],
-        quicksand: ["Quicksand", "sans-serif"]
+        quicksand: ["Quicksand", "sans-serif"],
       },
       boxShadow: {
         pinkBlur: "0 0 34px 0px rgba(255, 0, 255, 1)",
-        pinkBlur2: "0 0 17px 0px rgba(255, 0, 255, 1), inset 0 0 17px 0px rgba(255, 0, 255, 1)",
+        pinkBlur2:
+          "0 0 17px 0px rgba(255, 0, 255, 1), inset 0 0 17px 0px rgba(255, 0, 255, 1)",
         innerPinkBlur: " inset 0 0 34px 0px rgba(255, 0, 255, 1)",
         greenBlur: "0 0 17px 0px #86E409, inset 0 0 17px 0px #86E409",
         blueBlur: "0 0 17px 0px #4BFAF0, inset 0 0 17px 0px #4BFAF0",
